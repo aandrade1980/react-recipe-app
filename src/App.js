c4348954c1a9f769c9771ba2b5c7fa117456f4e9
alvaro-andrade <alvaro.andrade@salesforce.com>
2019-04-26 23:31:26 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+
+const API_KEY = '845558b03f426f8f1b46c3c916a58c11';
+const default_query = 'shredded%20chicken';
+
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  
+  const getRecipe = async e => {
+    e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value || default_query;
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    const data = await api_call.json();
+    console.log(data);
+    if(data.error === "limit") {
+      return setRecipes([]);
+    }
+    return setRecipes(data.recipes);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1 className="App-title">Recipe Search</h1>
       </header>
+      <Form getRecipe={ getRecipe } />
+      <Recipes recipes={ recipes } />
     </div>
   );
 }
